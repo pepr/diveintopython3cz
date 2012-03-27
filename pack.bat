827c1028ed72ebb67e93045ce3f879b58b99c472
Petr Prikryl <prikryl@atlas.cz>
2012-03-27 13:12:01 +0200

    Upravena dávka pack.py (single.html, .pdf)

--- pack.bat
+++ pack.bat
@@ -1,6 +1,6 @@
 pushd buildPDF
 copy PonormeSeDoPythonu3.pdf ..
-copy single.html PonormeSeDoPythonu3single.html
+copy single.html ..\PonormeSeDoPythonu3single.html
 popd
 rem Přibalí i vygenerovaný changelog.html a PonormeSeDoPythonu3single.html
 zip -r PonormeSeDoPythonu3-html *.html *.css examples/* i/* j/*
